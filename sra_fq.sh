4d1dcb5faaf596e265c46285a892a551e80881f3
Rahia Mashoodh <17982017+r-mashoodh@users.noreply.github.com>
2019-02-14 12:18:36 +0000

    Add files via upload

---
+++ sra_fq.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#sra toolkit
+
+# bioproject for shearwater
+# SRA Study:SRP171071
+
+cd sra_dump
+
+for i in `cat filelist.txt`;
+do
+  ../software/sratoolkit.2.9.2-ubuntu64/bin/fastq-dump --accession $i --outdir .
+done
+
+#../software/sratoolkit.2.9.2-ubuntu64/bin/vdb-dump -f tab -C READ $i | awk '{print ">" "$i." NR "\n" $0}' > $i.fasta
+
