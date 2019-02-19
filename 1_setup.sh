af6408ace4d4faeb62ac67e22b8043a84c2af251
Rahia Mashoodh <17982017+r-mashoodh@users.noreply.github.com>
2019-02-19 06:57:52 +0000

    Update 1_setup.sh

--- 1_setup.sh
+++ 1_setup.sh
@@ -15,25 +15,7 @@ unzip 0.20.0.zip
 # Download bowtie2
 git clone https://github.com/BenLangmead/bowtie2.git
 
-cd ..
-
-# Get some genomes
-
-mkdir genomes
-cd genomes
-
-# This is where you will place all the reference genomes you need
-
-# fish ... 
-# M. zebra
-# https://www.ncbi.nlm.nih.gov/genome?LinkName=bioproject_genome&from_uid=60369
-
-wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/238/955/GCF_000238955.4_M_zebra_UMD2a/GCF_000238955.4_M_zebra_UMD2a_genomic.fna.gz
-
-# spike-in
-# lamda for arne's libs
-# RRBS from diagenode website
-
+# possibly need to download samtools
 
 
 
