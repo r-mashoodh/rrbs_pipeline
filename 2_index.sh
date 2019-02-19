8f185c4b12c7e040c1769f64ab97f30b90e51476
Rahia Mashoodh <17982017+r-mashoodh@users.noreply.github.com>
2019-02-19 06:55:35 +0000

    Update 2_index.sh

--- 2_index.sh
+++ 2_index.sh
@@ -10,20 +10,30 @@ cd genomes
 # M. zebra
 # https://www.ncbi.nlm.nih.gov/genome?LinkName=bioproject_genome&from_uid=60369
 
+mdir m_zebra
+cd m_zebra
+
 wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/238/955/GCF_000238955.4_M_zebra_UMD2a/GCF_000238955.4_M_zebra_UMD2a_genomic.fna.gz
+
+cd .. 
 
 # spike-in
 # lamda for arne's libs
 # RRBS from diagenode website
 
+mkdir spike
+cd spike 
 
+# code to download spike in goes here
 
+cd ..
 
 # Create indices for both spike-in and fish
-# note made use of global installs of bowtie2 / samtools which are dependencies for bismark
+
+# you will need to replace 'genomes/m_zebra/' with the path to your 
 
 # genome preparation for m_zebra
-software/Bismark-0.20.0/bismark_genome_preparation genomes/m_zebra/
+software/Bismark-0.20.0/bismark_genome_preparation --path_to_bowtie ../software/bowtie2 genomes/m_zebra/
 
 # genome preparation for 
 software/Bismark-0.20.0/bismark_genome_preparation genomes/spike/
