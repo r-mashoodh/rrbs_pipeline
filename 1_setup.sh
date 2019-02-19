9d259d1d064364ff6cb4e4c3ee7dfd8bbf1ea7cb
Rahia Mashoodh <17982017+r-mashoodh@users.noreply.github.com>
2019-02-19 15:11:15 +0000

    added samtools/bowtie2 installation

--- 1_setup.sh
+++ 1_setup.sh
@@ -15,8 +15,13 @@ unzip 0.20.0.zip
 # Download bowtie2
 git clone https://github.com/BenLangmead/bowtie2.git
 
-# possibly need to download samtools
+# Download/install  samtools
+wget https://github.com/samtools/samtools/releases/download/1.9/samtools-1.9.tar.bz2 
+tar jxvf samtools-1.9.tar.bz2
 
+cd samtools-1.9
+./configure --prefix=`pwd`
+make
+make install
 
-
-
+cd .. #places you back into software folder
