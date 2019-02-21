7034f060031d56e3c284956dfd1de7ff6449aa9c
Rahia Mashoodh <17982017+r-mashoodh@users.noreply.github.com>
2019-02-21 11:39:40 +0000

    Update 3_trimming.sh

--- 3_trimming.sh
+++ 3_trimming.sh
@@ -3,14 +3,14 @@
 ## RAW READ STUFF
 
 mkdir trimmed_reads
-cd RRBS_sample
+cd sra_dump
 
 # 1_Trim galore
 
 # rrbs specific trimming
-for i in *.fq.gz;
+for i in *.fastq;
 do
-filename=`echo $i | awk -F '.fq.gz' '{print $1}'`
+filename=`echo $i | awk -F '.fastq' '{print $1}'`
 ../software/TrimGalore-0.5.0/trim_galore -rrbs -o ../trimmed_reads $filename".fq.gz"
 done
 
@@ -20,8 +20,9 @@ done
 mkdir fastqc_files
 cd trimmed_reads
 
-for i in *_trimmed.fq.gz;
+for i in *_trimmed.fastq;
 do
-filename =`echo $i | awk -F '_trimmed.fq.gz' '{print $1}'`
+filename =`echo $i | awk -F '_trimmed.fastq' '{print $1}'`
 fastqc $filename"_trimmed.fq.gz" -o ../fastqc_files
 done
+
